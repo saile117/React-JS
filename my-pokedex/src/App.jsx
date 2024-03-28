ca169b57dae073fc81dda26e7da8d91197c30c8f
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-03-28 17:26:35 +0100

    state.1

--- my-pokedex/src/App.jsx
+++ my-pokedex/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './components/PokemonCards'
 import PokemonCards from './components/PokemonCards'
@@ -8,34 +6,80 @@ import PokemonCards from './components/PokemonCards'
 
 const pokemonList = [
 
-{
+  {
 
-  name: "bulbasaur",
+    name: "bulbasaur",
 
-  imgSrc:
+    imgSrc:
 
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 
-},
+  },
 
-{
+  {
 
-  name: "mew",
+    name: "charmander",
 
-},
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+
+  },
+
+  {
+
+    name: "squirtle",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+
+  },
+
+  {
+
+    name: "pikachu",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+
+  },
+
+  {
+
+    name: "mew",
+
+  },
+
 ];
-function App() {
-  
 
-  
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickNext = () =>{
+    if (pokemonIndex === 4 ) {
+      console.log('yen a pas plus')
+    }
+    else setPokemonIndex(pokemonIndex + 1)
+
+  }
+  const handleClickPrevious = () => {
+    if (pokemonIndex === 0 ) {
+      console.log('yen a pas avant')
+    }
+
+     else setPokemonIndex(pokemonIndex - 1)
+  }
   return (
     <>
       <div>
-       <PokemonCards pokemon={pokemonList[0]}/>
-
+        <PokemonCards pokemon={pokemonList[pokemonIndex]} />
+        <button onClick={handleClickPrevious}>précédent</button>
+        <button onClick={handleClickNext}>suivant</button>
       </div>
-      
-      
+
+
     </>
   );
 }
--- my-pokedex/src/components/PokemonCards.jsx
+++ my-pokedex/src/components/PokemonCards.jsx
@@ -1,46 +1,31 @@
 import PropTypes from 'prop-types';
 
+   
+function PokemonCards({ pokemon }) {
 
-function PokemonCards({pokemon}) {
-
-  
- 
-
-
-  // const Pokemon = pokemonList[1];
-
-  
 
     return (
         <figure>
-          {pokemon.imgSrc? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
-          
-          <figcaption>
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+            <figcaption>
                 {pokemon.name}
 
-            </figcaption>
-            {/* <img
-                alt="bulbasaur"
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            />
-            <figcaption>
-                Bulbasaur
-
-            </figcaption> */}
+            </figcaption> 
         </figure>
+
     );
-  
+
 }
-PokemonCards.propTypes = 
+PokemonCards.propTypes =
 {
 
-pokemon: PropTypes.shape({
+    pokemon: PropTypes.shape({
 
-    name: PropTypes.string.isRequired,
-  
-    img: PropTypes.string,
-  
-}),
+        name: PropTypes.string.isRequired,
+
+        img: PropTypes.string,
+
+    }),
 };
 
 export default PokemonCards;
